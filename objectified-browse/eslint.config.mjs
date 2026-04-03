57baa69e5711b4334bd557f562f931b4ce6c948e
Ken Suenobu <ksuenobu@fastmail.com>
2026-04-02 20:39:12 -0600

    fix: 1.2 checklist - updates eslint coverage

--- objectified-browse/eslint.config.mjs
+++ objectified-browse/eslint.config.mjs
@@ -12,6 +12,9 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
+    // Generated and one-off scripts
+    "coverage/**",
+    "convert-mui-to-radix.js",
   ]),
 ]);
 
--- objectified-web/eslint.config.mjs
+++ objectified-web/eslint.config.mjs
@@ -5,11 +5,16 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  // Override default ignores of eslint-config-next.
   globalIgnores([
+    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
+    // Generated and one-off scripts
+    "coverage/**",
+    "convert-mui-to-radix.js",
   ]),
 ]);
 
